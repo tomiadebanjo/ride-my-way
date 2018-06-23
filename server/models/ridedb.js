112f30f0454107a8ec0711a585a71730ac3bc9fc
tomiadebanjo <tomiadebanjo@gmail.com>
2018-06-23 12:29:17 +0100

    Create api endpoints

---
+++ server/models/ridedb.js
@@ -0,0 +1,28 @@
+const db = [
+  {
+    id: 1,
+    driver: 'james',
+    destination: 'ikeja',
+    pickUpLocation: 'gbagada',
+    departureTime: '9:00AM',
+    requests:[]
+  },
+  {
+    id: 2,
+    driver: 'Ijeoma',
+    destination: 'Surulere',
+    pickUpLocation: 'mushin',
+    departureTime: '11:00AM',
+    requests:[]
+  },
+  {
+    id: 3,
+    driver: 'tochi',
+    destination: 'ikoyi',
+    pickUpLocation: 'Ijesha',
+    departureTime: '9:00AM',
+    requests:[]
+  }
+]
+
+export default db

7b46658951c7036eba4d9c8c60654a9d547ad09a
Adebanjo oluwatominiyin <tomiadebanjo@gmail.com>
2018-07-04 16:29:49 +0100

    feat(apiEndPoint) create endpoint to fetch all ride requests for a ride
    - create an endpoint to get all requests to join ride
    - create a controller to handle get all requests to join a specific ride
    [Delivers #158811375]

--- server/controllers/requestController.js
+++ server/controllers/requestController.js
@@ -10,6 +10,13 @@ const requestRide = (req, res) => {
   ];
   pool.query(text, values, (err, response) => {
     if (err) {
+      const duplicateKeyError = 'duplicate key value violates unique constraint \"ride_request_userid_rideid_key\"';
+      if(err.message === duplicateKeyError) {
+        return res.status(500).json({
+          success: false,
+          message: 'Error: You can only request for a ride once',
+        });
+      }
       return res.status(500).json({
         success: false,
         message: err.message,
@@ -23,6 +30,24 @@ const requestRide = (req, res) => {
   });
 };
 
+const rideRequests = (req, res) => {
+  const text = 'SELECT * FROM ride_request WHERE rideId = $1';
+  const values = [Number(req.params.rideId)];
+  pool.query(text, values, (err, response) => {
+    if (response.rows === undefined || response.rows.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: `${err.message} Enter valid Id`,
+      })
+    };
+    return res.status(200).json({
+      success: true,
+      requests: response.rows,
+    })
+  })
+} 
+
 export default {
   requestRide,
+  rideRequests,
 };

5b122df0c5a465b0f99ac34e38bd79afd8b5e703
Adebanjo oluwatominiyin <tomiadebanjo@gmail.com>
2018-07-05 03:02:38 +0100

    chore(validation): validate user inputs from endpoints
    - create a middleware to handle the validations for all endpoints requiring user input
    [Finishes #158828251]

---
+++ server/middleware/loginValidation.js
@@ -0,0 +1,38 @@
+const login = (req, res, next) => {
+  const charTest = /[a-zA-Z]/;
+  const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (
+    req.body.password === ''
+    || typeof req.body.password === 'undefined'
+    || req.body.password === null
+  ) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'password is required',
+    });
+  }
+  if (req.body.password.length < 6) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'Minimum password length is 6',
+    });
+  }
+  if (!charTest.test(req.body.password)) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'password must contain at least 1 aplahabet',
+    });
+  }
+  if (req.body.email === '' || !emailRegex.test(req.body.email)) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'Please enter a valid email address',
+    });
+  }
+  return next();
+};
+
+export default {
+  login,
+};
---
+++ server/middleware/mainValidation.js
@@ -0,0 +1,109 @@
+const newRide = (req, res, next) => {
+  const nonCharTest = /[^a-zA-Z/\s/-]/g;
+  const charTest = /[a-zA-Z]/g;
+  const timeTest = /^(([0-1]{0,1}[0-9])|(2[0-3])):[0-5]{0,1}[0-9]$/g;
+  const dateTest = /^\d{1,2}\/\d{1,2}\/\d{4}$/;
+
+  if (
+    req.body.destination === ''
+    || typeof req.body.destination === 'undefined'
+    || req.body.destination === null
+  ) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'destination field is required',
+    });
+  }
+  if (!charTest.test(req.body.destination)) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'destination must contain at least one alphabet',
+    });
+  }
+  if (nonCharTest.test(req.body.destination)) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'destination must be alphabetic, the use of spaces and "-" are allowed',
+    });
+  }
+  if (
+    req.body.pickUpLocation === ''
+    || typeof req.body.pickUpLocation === 'undefined'
+    || req.body.pickUpLocation === null
+  ) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'pickUpLocation field is required',
+    });
+  }
+  if (!charTest.test(req.body.pickUpLocation)) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'pickUpLocation must contain at least one alphabet',
+    });
+  }
+  if (nonCharTest.test(req.body.pickUpLocation)) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'pickUpLocation must be alphabetic, the use of spaces and "-" are allowed',
+    });
+  }
+  if (
+    req.body.departureTime === ''
+    || typeof req.body.departureTime === 'undefined'
+    || req.body.departureTime === null
+  ) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'departureTime field is required',
+    });
+  }
+  if (!timeTest.test(req.body.departureTime)) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'Invalid time format.. enter required format - hh:mm e.g:- 16:40',
+    });
+  }
+  if (
+    req.body.body.departureDate === ''
+    || typeof req.body.departureDate === 'undefined'
+    || req.body.departureDate === null
+  ) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'departureDate field is required',
+    });
+  }
+  if (!dateTest.test(req.body.departureDate)) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'Invalid Date format.. enter required format - dd/mm/yyyy e.g:- 10/05/2018',
+    });
+  }
+  return next();
+};
+
+const validateId = (req, res, next) => {
+  const rideId = Number(req.params.rideId);
+  const requestId = Number(req.params.requestId);
+  if (!Number.isInteger(rideId)) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'Invalid ride id!!',
+    });
+  }
+
+  if (!Number.isInteger(requestId)) {
+    return res.status(400).send({
+      success: 'false',
+      message: 'Invalid ride id!!',
+    });
+  }
+  return next();
+};
+
+
+export default {
+  newRide,
+  validateId,
+};
